da45d2c24039d24cc8fc2111834ca5b9d8e97d8b
yaroslavboreyko <yaroslav.boreyko@gmail.com>
2022-05-08 13:57:56 +0200

    Added Signin Signup pages. Integrated Auth into the Container. Added Initial state to memory history

--- packages/auth/config/webpack.prod.js
+++ packages/auth/config/webpack.prod.js
@@ -3,19 +3,18 @@ const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPl
 const commonConfig = require('./webpack.common');
 const packageJson = require('../package.json');
 
-const domain = process.env.PRODUCTION_DOMAIN;
-
 const prodConfig = {
     mode: 'production',
     output: {
         filename: '[name].[contenthash].js',
-        publicPath: '/container/latest/',
+        publicPath: '/marketing/latest/'
     },
     plugins: [
         new ModuleFederationPlugin({
-            name: 'container',
-            remotes: {
-                marketing: `marketing@${domain}/marketing/latest/remoteEntry.js`
+            name: 'auth',
+            filename: 'remoteEntry.js',
+            exposes: {
+                './AuthApp': './src/bootstrap'
             },
             shared: packageJson.dependencies,
         })
